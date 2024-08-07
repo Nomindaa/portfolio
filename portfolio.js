4198943d5a6cb4bbffbe285a41aed84d39db33af
Nomindaa <nbaterdene65@gmail.com>
2024-08-07 10:39:02 -0500

    first commit

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+let theme = "light";
+
+const themeButton = document.getElementById("logo-circle");
+
+function main() {
+  console.log("clicked");
+  if (theme === "light") {
+    theme = "dark";
+    const webTheme = document.getElementById("theme");
+    webTheme.setAttribute("href", "dark.css");
+  } else {
+    theme = "light";
+    const webTheme = document.getElementById("theme");
+    webTheme.setAttribute("href", "portfolio.css");
+  }
+}
+
+themeButton.addEventListener("click", main);
